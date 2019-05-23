9988f9365291fe881edfa86076c39d7285633e87
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-23 10:45:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function produceDrivingRange(range) {
+  return function (start, end) {
+    let diff = range - Math.abs(parseInt(start, 10) - parseInt(end, 10));
+    if (diff < 0) {
+      return `${Math.abs(diff)} blocks out of range`;
+    } else {
+      return `within range by ${diff}`;
+    }
+  };
+}
